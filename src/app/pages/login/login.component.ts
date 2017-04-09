ef3a832e22f57a53aa6318c1c0cc8ff9105fe3ac
Nguyen Phuong My <phuongmy57@gmail.com>
2017-04-10 08:13:52 +1000

    0.6.4
    login-stock mode

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -16,26 +16,34 @@ export class Login {
   public form:FormGroup;
   public email:AbstractControl;
   public password:AbstractControl;
+  public stock:boolean = false;
   public submitted:boolean = false;
 
   constructor(fb:FormBuilder, private auth: AuthService, private router:Router) {
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'stock': false
     });
 
     this.email = this.form.controls['email'];
     this.password = this.form.controls['password'];
+    this.stock = this.form.controls['stock'];
   }
 
   public onSubmit(values:Object):void {
     this.submitted = true;
-    if (this.form.valid) {
+    if (this.form.valid) {}
       // your code goes here
-      // console.log(values);
+
+       // console.log(values);
+       // debugger;
       this.auth.login().subscribe(res =>{
-        if(res.status === 200){
+        if(res.status === 200 && this.stock.value ==false){
           this.router.navigate(['pages']);
+        }
+        else if(res.status === 200 && this.stock.value){
+          this.router.navigate(['pages/mobiledashboard']);
         }
       })
     }
