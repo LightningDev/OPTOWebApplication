609a9ad6ab93fd9545a8fec9fb9e85b907ddd131
LightningDev <User@Users-MacBook-Pro.local>
2017-04-18 09:03:55 +1000

    Bin Location

---
+++ src/app/pages/location/binlocation.component.ts
@@ -0,0 +1,57 @@
+import {Component, ViewChild} from '@angular/core';
+import { BinLocationService } from '../../shared/services/binlocation.service';
+import { ModalDirective } from 'ng2-bootstrap';
+import 'style-loader!./buttons.scss';
+
+@Component({
+  selector: 'bin-location',
+  styleUrls: ['./modals.scss'],
+  templateUrl: './binlocation.html',
+})
+
+export class BinLocation {
+
+	@ViewChild('childModal') childModal: ModalDirective;
+
+	constructor(private service: BinLocationService) {
+		
+	}
+
+	showChildModal(): void {
+		this.childModal.show();
+	}
+
+	hideChildModal(): void {
+		this.childModal.hide();
+	}
+
+	button_OUT(event) {
+		//alert("OUT CLICKED");
+		let json = {
+			"material_code" : "",
+			"barcode": this.inputBarcode,
+			"bin_location" : this.inputBinLocation,
+			"action" : "0"
+		} 
+		this.service.sendBinLocation(json).subscribe(res => {
+    		//alert(res.json().message);
+
+    	})
+	}
+
+	button_IN(event) {
+		//alert("IN CLICKED");
+		let json = {
+			"material_code" : "",
+			"barcode": this.inputBarcode,
+			"bin_location" : this.inputBinLocation,
+			"action" : "1"
+
+		} 
+		this.service.sendBinLocation(json).subscribe(res => {
+    		//alert(res.json().message);
+    		
+    	})
+	}
+
+}
