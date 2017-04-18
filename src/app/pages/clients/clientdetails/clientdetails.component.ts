d0ba33edb383f46f6300a7f7a00ece633b46f603
LightningDev <User@Users-MacBook-Pro.local>
2017-04-18 12:49:53 +1000

    Client Google Map

--- src/app/pages/clients/clientdetails/clientdetails.component.ts
+++ src/app/pages/clients/clientdetails/clientdetails.component.ts
@@ -11,17 +11,23 @@ import { ClientService } from '../../../shared/services/client.service';
 import * as GoogleMapsLoader from 'google-maps';
 
 @Component({
-  selector: 'client-details',
-  styleUrls: ['./googleMaps.scss'],
-  templateUrl: './clientdetails.html',
+	selector: 'client-details',
+	styleUrls: ['./googleMaps.scss'],
+	templateUrl: './clientdetails.html',
 })
 
 export class ClientDetails {
+
 	constructor(private route: ActivatedRoute, private location: Location, private service: ClientService, private _elementRef:ElementRef) {
-		
+		//loadMap();	
 	}
 
 	ngOnInit() {
+
+	}
+
+	ngAfterViewInit() {
+		let el = this._elementRef.nativeElement.querySelector('.google-maps');
 		this.route.params
 		.switchMap((params: Params) => this.service.getClientDetails(params['id']))
 		.subscribe(res => {
@@ -39,20 +45,39 @@ export class ClientDetails {
 			this.inputDeliveryState = res.json()["items"][0]["state"];
 			this.inputPostalPostcode = res.json()["items"][0]["postcode"];
 			this.inputDeliveryPostcode = res.json()["items"][0]["postal_postcode"];
+			
+			// Address
+			var address = res.json()["items"][0]["address1"] + "," + res.json()["items"][0]["address2"] + "," + res.json()["items"][0]["city"] + "," + res.json()["items"][0]["state"];
+			
+			// Load Google Map
+			loadGoogleMap(el, address);
+		}
+	}
+
+	function loadGoogleMap(el, address) {
+		GoogleMapsLoader.load((google) => {
+			var map = new google.maps.Map(el, {
+				center: new google.maps.LatLng(-26.9968449, 153.3178702),
+				zoom: 16,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			});
+			var geocoder = new google.maps.Geocoder();
+			geocodeAddress(geocoder, map, address);
 		});
 	}
 
-	ngAfterViewInit() {
-	    let el = this._elementRef.nativeElement.querySelector('.google-maps');
-
-	    // TODO: do not load this each time as we already have the library after first attempt
-	    GoogleMapsLoader.load((google) => {
-	      new google.maps.Map(el, {
-	        center: new google.maps.LatLng(44.5403, -78.5463),
-	        zoom: 8,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
-	      });
-    });
-  }
+	function geocodeAddress(geocoder, resultsMap, address) {
+		geocoder.geocode({'address': address}, function(results, status) {
+			if (status === 'OK') {
+				resultsMap.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: resultsMap,
+					position: results[0].geometry.location
+				});
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+	}
 
 }
