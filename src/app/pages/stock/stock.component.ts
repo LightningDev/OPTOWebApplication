2dfcee616a111e2a94285ce73001b2e1c5ea05ba
Nguyen Phuong My <phuongmy57@gmail.com>
2017-04-27 09:30:27 +1000

    update stock form controller

--- src/app/pages/stock/stock.component.ts
+++ src/app/pages/stock/stock.component.ts
@@ -1,7 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
+import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 import { StockService } from '../../shared/services/stock.service';
 import { ModalDirective } from 'ng2-bootstrap';
 import 'style-loader!./buttons.scss';
+
+import {Router} from '@angular/router'
+import {AuthService} from "../../shared/services/auth.service";
 
 @Component({
   selector: 'stock',
@@ -11,11 +15,25 @@ import 'style-loader!./buttons.scss';
 
 export class Stock {
 
+	public form:FormGroup;
+	public stockTake:boolean = false;
+	public stockInOut:boolean = false;
+	public stockAdjustment:boolean = false;
+
 	@ViewChild('childModal') childModal: ModalDirective;
 
-	constructor(private service: StockService) {
-		
-	}
+	constructor(fb:FormBuilder, private auth: AuthService, private router:Router) {
+    this.form = fb.group({
+      'stockTake': false,
+      'stockInOut': false
+      'stockAdjustment': false
+    });
+
+    this.stockTake = this.form.controls['stockTake'];
+    this.stockInOut = this.form.controls['stockInOut'];
+    this.stockAdjustment = this.form.controls['stockAdjustment'];
+  }
+
 
 	// showChildModal(): void {
 	// 	this.childModal.show();
--- src/app/pages/stock/stock.module.ts
+++ src/app/pages/stock/stock.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { StockService } from '../../shared/services/stock.service';
@@ -12,6 +12,7 @@ import { Stock } from './stock.component';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     NgaModule,
     Ng2SmartTableModule,
