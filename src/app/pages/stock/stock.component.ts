2ce272a4a87845722bd5b01eca466aba0d77234b
Nguyen Phuong My <phuongmy57@gmail.com>
2017-05-04 13:39:52 +1000

    update stock version 1.1

--- src/app/pages/stock/stock.component.ts
+++ src/app/pages/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, FORM_DIRECTIVES, NgFor} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 
 import { DivisionService } from '../../shared/services/division.service';
@@ -22,16 +22,19 @@ export class Stock {
 
 	public inputBarcode:string='';
 	public currmat:string='';
-	public noti:string='';
+	public stockType:string='';
+	
 	public success:string='';
+	public noti:string='';
+	public empNoti:string='';
+	public stockNoti:string='';
 	// public inputCurrent:number;
 	// public inputOnHand:number;
 
-	public division:Object;
-	public employee:Object;
-	public emplist:Object;
+	public divisionsList:Object;
+	public employeeList:Object;
 
-	public check:boolean = false;
+
 	public stockTake:boolean = false;
 	public stockInOut:boolean = false;
 	public stockAdjustment:boolean = false;
@@ -48,8 +51,8 @@ export class Stock {
 
     this.form = fb.group({
       'stockTake': false,
-      'stockInOut': false
-      'stockAdjustment': false
+      'stockInOut': false,
+      'stockAdjustment': false,
     });
 
 
@@ -64,35 +67,22 @@ export class Stock {
 
     	this.divisionservice.getDivision().subscribe(res => {
         //alert(JSON.stringify(res.json()));
-        this.divisions = res.json()["items"];
+        this.divisionsList = res.json()["items"];
         // console.log(this.divisions);
         // debugger;
     	})
 
     	this.employeeservice.getEmployee().subscribe(res => {
         //alert(JSON.stringify(res.json()));
-        this.employees = res.json()["employees"];
+        this.employeeList = res.json()["employees"];
         // console.log(this.divisions);
         // debugger;
     	})
 
-    	// this.materialservice.getMaterialCatalogue().subscribe(res =>{
-    	// this.matlist = res.json()["items"];
-    	// // console.log(this.emplist);
-    	// // debugger;
-    	// })
-
   	}
 
 	checkMaterialCode(inputBarcode): void{
-		// for (var mat in this.matlist){
-		// 	if(this.matlist[mat].ID == inputBarcode){
-		// 			this.materialservice.getMaterialDetails(inputBarcode).subscribe(res =>{
-  //   				this.currmat = res.json()["items"][0]});
-  //   				this.check = true;
-  //   				console.log(this.matlist[mat].ID);
-		// 	}
-		// }
+	
 		if(inputBarcode==" " || inputBarcode=="" || inputBarcode==null){
 			this.inputBarcode = inputBarcode;
 			this.noti = "(BarCode required)";
@@ -108,10 +98,64 @@ export class Stock {
 	    		this.currmat = res.json()["items"][0]["cash_p_m"];
 	    		this.noti= "";
 	    		this.success ="valid";
+	    		debugger;
+	    		let abc = this.vc;
 	    		}
 			})
 		}
   	}
+
+  	process(event): void{
+  // 		if(this.divisionCode==" " || this.divisionCode=="" || this.divisionCode==null){
+		// 	this.divNoti = "(Please select division)";
+		// 	return;
+		// }
+		if(this.divisionCode==null||this.divisionCode=='undefined'||this.divisionCode==''||this.divisionCode==""){
+			this.divisionCode='';
+		}else{
+			console.log("divisionCode");
+		}
+
+		if(this.employeeCode==null||this.employeeCode=='undefined'||this.employeeCode==''){
+			this.employeeCode='';
+			this.empNoti='(Please select an employee)';
+		}else{
+			console.log("employeeCode");
+			this.empNoti='';
+		}
+
+
+		// if(this.stockTake.value==false && this.stockInOut.value==false && this.stockAdjustment.value==false){
+		// 	this.stockNoti='(Please select stock type)';
+		// 	return;
+		// }else{
+			
+		// 	if(this.stockInOut.value!=false && this.stockType!=this.stockInOut.value){
+		// 		this.stockType=this.stockInOut.value;
+		// 		console.log('abc');
+		// 		console.log(this.stockInOut.value);
+		// 	}
+		// 	else if(this.stockAdjustment.value!=false && this.stockType!=this.form.controls['stockAdjustment']){
+		// 		this.stockType=this.stockAdjustment.value;
+		// 		console.log('xyz');
+		// 		console.log(this.stockAdjustment.value);
+		// 	}
+		// 	else if(this.stockTake.value!=false && this.stockType!=this.form.controls['stockTake']){
+		// 		this.stockType=this.stockTake.value;
+		// 		console.log('qwe');
+		// 		console.log(this.stockTake.value);
+		// 	}
+		// }
+		// console.log("-----");
+		// console.log(this.stockType!==this.stockTake.value);
+		// console.log(this.stockType!==this.stockAdjustment.value);
+		// console.log(this.stockType!==this.stockInOut.value);
+		// console.log(this.stockType);
+		// console.log(this.stockTake.value);
+		// console.log(this.stockInOut.value);
+		// console.log(this.stockAdjustment.value);
+		debugger;
+	}
 
 
 }
