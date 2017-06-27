c2b6ef63274e8c968616f360b666169e3e55ddb0
Nguyen Phuong My <phuongmy57@gmail.com>
2017-06-27 14:59:04 +1000

    update UI for mobile and desktop

--- src/app/pages/pallet/pallet.component.ts
+++ src/app/pages/pallet/pallet.component.ts
@@ -3,9 +3,9 @@ import { PalletService } from '../../shared/services/pallet.service';
 import 'style-loader!./buttons.scss';
 
 @Component({
-  selector: 'pallet',
-  styleUrls: ['./modals.scss'],
-  templateUrl: './pallet.html',
+	selector: 'pallet',
+	styleUrls: ['./modals.scss'],
+	templateUrl: './pallet.html',
 })
 
 export class Pallet implements AfterViewInit{
@@ -14,6 +14,7 @@ export class Pallet implements AfterViewInit{
 	inputPallet:string='';
 	inputBinLocation:string='';
 	inputJob:string='';
+	inout:string='in';
 
 	constructor(private service: PalletService) {
 		
@@ -21,71 +22,82 @@ export class Pallet implements AfterViewInit{
 
 	ngAfterViewInit(){
 		if(screen.width > 420){
-      		document.getElementsByClassName('col-xs-6')['0'].style.width = '150px';
-      		document.getElementsByClassName('col-xs-6')['1'].style.width = '150px';
-      		document.getElementsByClassName('form-group')['0'].style.width = '300px';
-      		document.getElementsByClassName('form-group')['1'].style.width = '300px';
-      		document.getElementsByClassName('widgets')['0'].style.width = '400px';
-    	}
-  	}
-
-	radio_stock(event) {    
-	    this.currentRadio = event.currentTarget.defaultValue;
-	    if(this.currentRadio=="PalletToLocation"){
-	    	 document.getElementById('PalletToLocation').style.display = 'block';
-             document.getElementById('JobToPallet').style.display = 'none';
-	     }
-
-	     if(this.currentRadio=="JobToPallet"){
-	     	 document.getElementById('PalletToLocation').style.display = 'none';
-             document.getElementById('JobToPallet').style.display = 'block';
-	     }
-  	}
-
-
-
-	button_OUT(event) {
-		//alert("OUT CLICKED");
-		let json = {
-			"pallet_code": this.inputPallet,
-			"bin_location" : this.inputBinLocation,
-			"job":this.inputJob,
-			"action" : "0"
-		} 
-		this.service.sendPallet(json).subscribe(res => {
-			if(this.inputBinLocation == "" && this.currentRadio == "PalletToLocation"){
-				alert("Input location field cannot be empty");
-			}else if(this.inputJob == "" && this.currentRadio == "JobToPallet"){
-				alert("Input job field cannot be empty");
-			}else{
-	    		alert(res.json().message);
-				this.inputJob="";
-				this.inputBinLocation="";
-			}
-    	})
-
+			document.getElementsByClassName('col-xs-6')['0'].style.width = '150px';
+			document.getElementsByClassName('col-xs-6')['1'].style.width = '150px';
+			document.getElementsByClassName('form-group')['0'].style.width = '300px';
+			document.getElementsByClassName('form-group')['1'].style.width = '300px';
+			document.getElementsByClassName('widgets')['0'].style.width = '400px';
+		}
 	}
 
-	button_IN(event) {
-		//alert("IN CLICKED");
+	radio_stock(event) {    
+		this.currentRadio = event.currentTarget.defaultValue;
+		if(this.currentRadio=="PalletToLocation"){
+			document.getElementById('PalletToLocation').style.display = 'block';
+			document.getElementById('JobToPallet').style.display = 'none';
+		}
+
+		if(this.currentRadio=="JobToPallet"){
+			document.getElementById('PalletToLocation').style.display = 'none';
+			document.getElementById('JobToPallet').style.display = 'block';
+		}
+	}
+
+	Switch(event) {
+		if(this.inout=="in"){
+			this.inout="out";
+		}else{
+			this.inout="in";
+		}
+		console.log(this.inout);
+	}
+
+	Update(event)	{
 		let json = {
 			"pallet_code": this.inputPallet,
 			"bin_location" : this.inputBinLocation,
 			"job":this.inputJob,
-			"action" : "1"
+			"action" : this.inout,
+		}
+		this.inputPallet = this.inputPallet.replace(/\s/g, '');
 
-		} 
-		this.service.sendPallet(json).subscribe(res => {
-    		if(this.inputBinLocation == "" && this.currentRadio == "PalletToLocation"){
-				alert("Input location field cannot be empty");
-			}else if(this.inputJob == "" && this.currentRadio == "JobToPallet"){
-				alert("Input job field cannot be empty");
+		if(this.inputPallet==""){
+			alert("Input pallet cannot be empty");
+		}else{
+			if(this.inout=="in"){
+				if(this.inputBinLocation == "" && this.currentRadio == "PalletToLocation"){
+					alert("Input location field cannot be empty");
+					return;
+				}else if(this.inputJob == "" && this.currentRadio == "JobToPallet"){
+					alert("Input job field cannot be empty");
+					return;
+				}else{
+					this.service.sendPallet(json).subscribe(res => {
+						alert(res.json().message);
+						this.inputJob="";
+						this.inputBinLocation="";
+					})	
+				}
+
 			}else{
-	    		alert(res.json().message);
-				this.inputJob="";
-				this.inputBinLocation="";
+				
+				if(this.inputBinLocation == "" && this.currentRadio == "PalletToLocation"){
+					alert("Input location field cannot be empty");
+				}
+				if(this.inputJob == "" && this.currentRadio == "JobToPallet"){
+					alert("Input job field cannot be empty");
+				}else{
+					this.service.sendPallet(json).subscribe(res => {
+						alert(res.json().message);
+						this.inputJob="";
+						this.inputBinLocation="";
+					})
+				}
+				
 			}
-    	})
+		}
+
+	}
 
 	}
 
